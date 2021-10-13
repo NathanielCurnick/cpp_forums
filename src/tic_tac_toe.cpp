dd1645d55f0f01c9e59b93d6bc313d74ddb63e0d
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2021-10-13 05:24:16 +0100

    feat: Added game victory checks

--- src/tic_tac_toe.cpp
+++ src/tic_tac_toe.cpp
@@ -34,8 +34,11 @@ int change_board(char *board, int move, bool player1_move)
     }
 }
 
-int game_won()
+int game_won(char *board)
 {
+    // Return 0 if game not won
+    // Return 1 if player 1 won
+    // Return 2 is player 2 won
     // Game ended check
     // Winning states
     // 1,2,3
@@ -46,6 +49,46 @@ int game_won()
     // 3,6,9
     // 1,5,9
     // 3,5,7
+
+    // check horizontals
+
+    for (int i = 0; i < 9; i += 3)
+    {
+        if (*(board + i) == 'X' && *(board + i + 1) == 'X' && *(board + i + 2) == 'X')
+        {
+            return 1;
+        }
+        else if (*(board + i) == 'O' && *(board + i + 1) == 'O' && *(board + i + 2) == 'O')
+        {
+            return 2;
+        }
+    }
+
+    // Check verticles
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (*(board + i) == 'X' && *(board + i + 3) == 'X' && *(board + i + 6) == 'X')
+        {
+            return 1;
+        }
+        else if (*(board + i) == 'O' && *(board + i + 3) == 'O' && *(board + i + 6) == 'O')
+        {
+            return 2;
+        }
+    }
+
+    // Check diagonals
+
+    if ((*(board) == 'X' && *(board + 4) == 'X' && *(board + 8) == 'X') || (*(board + 2) == 'X' && *(board + 4) == 'X' && *(board + 6)))
+    {
+        return 1;
+    }
+    else if ((*(board) == 'O' && *(board + 4) == 'O' && *(board + 8) == 'O') || (*(board + 2) == 'O' && *(board + 4) == 'O' && *(board + 6) == 'O'))
+    {
+        return 2;
+    }
+    return 0;
 }
 
 bool board_full(char *board)
@@ -109,5 +152,18 @@ int main()
         }
 
         // Give message if someone wins
+        int won = game_won(board);
+        if (won == 1)
+        {
+            cout << "Player 1 wins" << std::endl;
+            game_running = false;
+            break;
+        }
+        else if (won == 2)
+        {
+            cout << "Player 2 wins" << std::endl;
+            game_running = false;
+            break;
+        }
     }
 }
