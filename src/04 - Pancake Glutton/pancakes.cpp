8cc3c48749bd91d574a91aaab7285993b5e68bac
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2022-01-22 16:41:58 +0000

    feat: Pancakes and number guessing now work

--- src/04 - Pancake Glutton/pancakes.cpp
+++ src/04 - Pancake Glutton/pancakes.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 using std::cin;
 using std::cout;
+using std::endl;
+using std::sort;
+using std::vector;
 
-struct pancakes
+struct Pancakes
 {
-    /* data */
+    int person;
+    int num;
 };
 // TODO
 
-int main(){
-    for (int i = 1; i <= 10; i++) {
-        cout << "Please enter the number of pancakes ate by person " << i;
+void print_pancakes(vector<Pancakes> &vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        cout << "Perosn " << vec.at(i).person << " ate " << vec.at(i).num << " pancakes" << endl;
+    }
+}
+
+bool pancake_sort(Pancakes p1, Pancakes p2)
+{
+    return p1.num > p2.num;
+}
+
+int main()
+{
+    vector<Pancakes> data = {};
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Please enter the number of pancakes ate by person " << i << " ";
         int number_ate;
         cin >> number_ate;
+
+        data.insert(data.end(), {i, number_ate});
     }
+
+    sort(data.begin(), data.end(), pancake_sort);
+
+    print_pancakes(data);
 }
--- src/05 - Bracketing Search/number_guess.cpp
+++ src/05 - Bracketing Search/number_guess.cpp
@@ -3,43 +3,65 @@
 using std::cin;
 using std::cout;
 
-int human_guessing() {
+void human_guessing()
+{
     srand(time(NULL));
     int goal = rand() % 100 + 1;
     int guess;
-    do {
+    do
+    {
         cout << "Guess the number (1 to 100) \n";
-        
+
         cin >> guess;
-        if (guess < goal) cout << "The goal number is higher \n";
-        else if (goal < guess) cout << "The goal number is lower \n";
+        if (guess < goal)
+            cout << "The goal number is higher \n";
+        else if (goal < guess)
+            cout << "The goal number is lower \n";
     } while (goal != guess);
 
-    cout << "You guessed the number (" << goal << ")";
+    cout << "You guessed the number (" << goal << ")\n";
 }
 
-int computer_guessing() {
+void computer_guessing()
+{
     srand(time(NULL));
     bool guessing = true;
     cout << "Think of a number between 0 and 100 \n";
     int computer_guess = 50;
     int user_input;
-    while (guessing) {
-        cout << "My guess is " << computer_guess << " is this right? \n";
+    int current_high = 100;
+    int current_low = 0;
+    while (guessing)
+    {
+        cout << "My guess is " << computer_guess << "; is this right? \n";
         cout << "1 - yes \n 2 - higher \n 3 - lower";
         cin >> user_input;
 
-        switch (user_input) {
-            case 1:
-                cout << "I won!";
-                guessing = false;
-                break;
-            case 2:
-            // TODO
+        switch (user_input)
+        {
+        case 1:
+            cout << "I won!\n";
+            guessing = false;
+            break;
+        case 2:
+            current_low = computer_guess;
+            computer_guess = (current_high + current_low) / 2;
+            break;
+        case 3:
+            current_high = computer_guess;
+            computer_guess = (current_high + current_low) / 2;
+            break;
+        default:
+            break;
         }
     }
 }
 
-int main() {
-    
+int main()
+{
+    cout << "Welcome to the guessing game!\n";
+    cout << "First you will guess the computer number, then it will guess yours\n";
+
+    human_guessing();
+    computer_guessing();
 }
